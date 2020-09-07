5986f539479e9d0fd117c480eb5a0f19af6c60e0
Alexander Sjögren <alle656@hotmail.com>
2020-09-07 17:02:58 +0200

    Fungerar bra nu, städning på css kvar

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const name = "test"; // Användarnamnet som används för att logga in
 const password = "1234";    // Lösen som används för inloggning
 
-const btn = document.getElementById("btn"); 
+const btn = document.getElementById("btn");  // hämtar in mina element med hjälp av deras id med getElementById metoden.
 const logInHeader = document.getElementById("loginHeader");
-let userName = document.getElementById("userName");
-let userPassword = document.getElementById("password");
 const outputDiv = document.getElementById("output");
 const inputDiv = document.getElementById("input");
 
-const logoutBtn = document.createElement("button"); // skapar min knapp för utloggning.
-    logoutBtn.id = "logoutBtn";
-    logoutBtn.innerHTML = "Logout";
+let userName = document.getElementById("userName");
+let userPassword = document.getElementById("password");
 
     // Gör en kontroll för att se om vi har något i localstorage.
     if (localStorage.length > 0) {
@@ -19,38 +16,28 @@ const logoutBtn = document.createElement("button"); // skapar min knapp för utl
     else {
         resetInputdiv(); // Annars kastar vi upp inloggningsformuläret.
     }
-
 // Kollar så name och password stämmer. 
 function logIn () {
     let userNameInput = userName.value; // Hämtar användarnamn från input
     let userPasswordInput = userPassword.value;
-    console.log(userNameInput)
-    console.log(userPasswordInput);
     checkingUserInput(userNameInput, userPasswordInput)
 }
-// visar sidan för inloggad
+// visar sidan för inloggad, används inuti kontrollen av localstorage + checkingUserInput(). 
 function loggedInSuccessfull () {
-   // logInHeader.innerHTML = "Logged in as, " + name;
-   inputDiv.innerHTML = "";
-    outputDiv.innerHTML = "Login successfull, welcome " + name; // sätter innehåll i min tomma div.
+    inputDiv.innerHTML = "";
+    outputDiv.innerHTML = "<h4>Login successfull, welcome back!<h4/>" // sätter innehåll i min tomma div.
     localStorage.setItem("namn", name); // lägger till användarnamn i localstorage
     addLogoutButton();
     logout();
-    console.log("lyckad inloggning");
 }
 // visar sida för misslyckad inloggning
 function logInFailed() {
-   // logInHeader.innerHTML = "";
-    outputDiv.innerHTML = "Login failed, please try again!"
-    console.log("misslyckad inloggning");
+    outputDiv.innerHTML = "<h5>Login failed, please try again!<h5/>"
 }
 // funktion som aktiveras och loggar ut när vi trycker på logout.
 function logout () {   
-    logoutBtn.onclick = function () {  // funktion som tömmer innerhtml när vi trycker på logout.
+    logoutBtn.onclick = function () {  // funktion som tömmer innerhtml + localstorage när vi trycker på logout.
         localStorage.clear(); // Tömmer localstorage
-        logInHeader.innerHTML = "Please log in!";
-        outputDiv.innerHTML = "";
-        console.log("click på logoutknapp");
         resetInputdiv();
     }
 }
@@ -63,15 +50,18 @@ function checkingUserInput(namn, losen) {  // bör hitta andra namn på parametr
         logInFailed();
     }
 }
-function addLogoutButton (){
+function addLogoutButton (){ // Lägger till utloggningsknappen, denna används inuti loggedInSuccessfull().
+    const logoutBtn = document.createElement("button"); // skapar min knapp för utloggning.
+    logoutBtn.id = "logoutBtn";
+    logoutBtn.innerHTML = "Logout";
     outputDiv.appendChild(logoutBtn);
-    console.log("logout test");
 }
 // Denna funktion återställer sidan till sitt ursprung, den används inuti logout funktionen.
 function resetInputdiv () {
-    inputDiv.innerHTML = "<h4 id='loginHeader'>Please log in!</h4>" +
-    "<input type='text' id='userName' placeholder='Enter username...'>" + // skapar mina input fält, submit knapp på nytt                                                                                           
-    "<input type='password' id='password' placeholder='Enter password...'>" + 
+    outputDiv.innerHTML = "";
+    inputDiv.innerHTML = "<h4 id='loginHeader'>Please enter your Username and Password</h4>" + " " +
+    "<input type='text' id='userName' placeholder='Enter username...'>" + " " + // skapar mina input fält, submit knapp på nytt                                                                                           
+    "<input type='password' id='password' placeholder='Enter password...'>" + " " +
     "<button id='btn' onclick='logIn();'>Submit</button>";  
     userName = document.getElementById("userName"); // fick hämta in id på nytt annars låg mina gamla values kvar.
     userPassword = document.getElementById("password"); // fick hämta in id på nytt annars låg mina gamla values kvar.
