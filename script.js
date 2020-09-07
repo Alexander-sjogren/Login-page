8f02e4e5b5d6faa26cb323a6318d5c3413f1b9ad
Alexander Sjögren <alle656@hotmail.com>
2020-09-07 14:00:16 +0200

    uppdaterar problem med inloggning efter utloggning får ej till uppdatering av .value

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const name = "test"; // Användarnamnet som används för att logga in
 const password = "1234";    // Lösen som används för inloggning
+
 const btn = document.getElementById("btn"); 
-const userName = document.getElementById("userName");
-const userPassword = document.getElementById("password");
+const logInHeader = document.getElementById("loginHeader");
+let userName = document.getElementById("userName");
+let userPassword = document.getElementById("password");
 const outputDiv = document.getElementById("output");
+const inputDiv = document.getElementById("input");
 
 const logoutBtn = document.createElement("button"); // skapar min knapp för utloggning.
     logoutBtn.id = "logoutBtn";
@@ -21,6 +24,8 @@ function logIn () {
 }
 // visar sidan för inloggad
 function loggedInSuccessfull () {
+   // logInHeader.innerHTML = "Logged in as, " + name;
+   inputDiv.innerHTML = "";
     outputDiv.innerHTML = "Login successfull, welcome " + name; // sätter innehåll i min tomma div.
     localStorage.setItem("namn", name); // lägger till användarnamn i localstorage
     addLogoutButton();
@@ -29,15 +34,18 @@ function loggedInSuccessfull () {
 }
 // visar sida för misslyckad inloggning
 function logInFailed() {
+   // logInHeader.innerHTML = "";
     outputDiv.innerHTML = "Login failed, please try again!"
     console.log("misslyckad inloggning");
 }
 // funktion som aktiveras och loggar ut när vi trycker på logout.
-function logout () {
-    localStorage.removeItem(name); // Tömmer localstorage
-    logoutBtn.onclick = function () {
+function logout () {   
+    logoutBtn.onclick = function () {  // funktion som tömmer innerhtml när vi trycker på logout.
+        localStorage.clear(); // Tömmer localstorage
+        logInHeader.innerHTML = "Please log in!";
         outputDiv.innerHTML = "";
         console.log("click på logoutknapp");
+        resetInputdiv();
     }
 }
 // Funktion som kollar användarnamn och lösenord
@@ -52,4 +60,9 @@ function checkingUserInput(namn, losen) {  // bör hitta andra namn på parametr
 function addLogoutButton (){
     outputDiv.appendChild(logoutBtn);
     console.log("logout test");
+}
+function resetInputdiv () {
+    inputDiv.innerHTML ="<input type='text' id='userName' placeholder='Enter username...'>" + 
+    "<input type='password' id='password' placeholder='Enter password...'>" + 
+    "<button id='btn' onclick='logIn();'>Submit</button>";
 }
